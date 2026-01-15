3b619ff0a668efe189b176c407678588b6d82525
Roberto Garcia <roberto.garcia@tekntek.com>
2026-01-15 14:58:05 -0600

    CountSemiprimes: use integer-only i*i bound in sieve

--- Lesson 11 Sieve of Eratosthenes/ CountSemiprimes.py
+++ Lesson 11 Sieve of Eratosthenes/ CountSemiprimes.py
@@ -5,7 +5,9 @@ def solution(N, P, Q):
     # Build smallest prime factor (SPF) array
     spf = [0] * (N + 1)
 
-    for i in range(2, int(N ** 0.5) + 1):
+    for i in range(2, N + 1):
+        if i * i > N:
+            break
         if spf[i] == 0:              # i is prime
             for j in range(i * i, N + 1, i):
                 if spf[j] == 0:
